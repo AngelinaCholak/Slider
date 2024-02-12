85093f55c46d12512ffbf6956b44a07cb85b6dc8
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-02-12 22:19:21 +0400

    Slider

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const images = document.querySelectorAll(".slider .slider-line img");
-const sliderLine = document.querySelector(".slider-line");
+const sliderLine = document.querySelector(".slider .slider-line");
 let count = 0;
 let width;
-let startX = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
 function init() {
+  console.log("resize");
   width = document.querySelector(".slider").offsetWidth;
   sliderLine.style.width = width * images.length + "px";
   images.forEach((item) => {
@@ -33,34 +35,31 @@ document.querySelector(".slider-prev").addEventListener("click", function () {
   rollSlider();
 });
 
+
+sliderLine.addEventListener("touchstart", function (e) {
+  touchStartX = e.touches[0].clientX;
+});
+
+sliderLine.addEventListener("touchend", function (e) {
+  touchEndX = e.changedTouches[0].clientX;
+  handleGesture();
+});
+
+function handleGesture() {
+  if (touchEndX < touchStartX) {
+    count++;
+    if (count >= images.length) {
+      count = 0;
+    }
+  } else if (touchEndX > touchStartX) {
+    count--;
+    if (count < 0) {
+      count = images.length - 1;
+    }
+  }
+  rollSlider();
+}
+
 function rollSlider() {
   sliderLine.style.transform = "translate(-" + count * width + "px)";
-}
-
-sliderLine.addEventListener("touchstart", handleTouchStart);
-sliderLine.addEventListener("touchmove", handleTouchMove);
-sliderLine.addEventListener("touchend", handleTouchEnd);
-
-function handleTouchStart(event) {
-  startX = event.touches[0].clientX;
-}
-
-function handleTouchMove(event) {
-  const moveX = event.touches[0].clientX;
-  const diffX = startX - moveX;
-  if (Math.abs(diffX) > 100) {
-    if (diffX > 0) {
-      count++;
-      if (count >= images.length) {
-        count = 0;
-      }
-    } else {
-      count--;
-      if (count < 0) {
-        count = images.length - 1;
-      }
-    }
-    rollSlider();
-    startX = moveX;
-  }
 }
